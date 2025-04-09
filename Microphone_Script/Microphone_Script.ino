6d3b35e7c634915a68273ca0c1913d38f297a605
Mpendulo <131270532+Gifted-one@users.noreply.github.com>
2025-04-09 15:22:40 +0200

    Initialized with a .ino file

---
+++ Microphone_Script/Microphone_Script.ino
@@ -0,0 +1,11 @@
+
+//This is a holder file we are going to populate later
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
